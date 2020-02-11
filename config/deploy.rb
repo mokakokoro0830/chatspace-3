384142cc3203561796a9f9aab70db53596518c4e
shogo <mokakokoro0830@gmail.com>
2020-02-11 20:16:55 +0900

    application name changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.11.2'
 
 # Capistranoのログの表示に利用する
-set :application, 'chatspace-3'
+set :application, 'chat-space3のコピー練習用'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:mokakokoro0830/chatspace-3.git'
